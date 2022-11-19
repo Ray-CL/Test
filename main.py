0cab0189581455ff38734687b4642f29fd03a38a
Ray <cool.dream@qq.com>
2022-11-19 19:12:55 +0800

    删除所有

--- main.py
+++
@@ -1,5 +0,0 @@
-# -*- coding=utf-8 -*-
-# Time:2022/11/19 18:14
-# Author:Ray
-# File:main.py
-# Software:PyCharm
--- shixun/test.py
+++
@@ -1,5 +0,0 @@
-import urllib.request
-
-url = "http://www.baidu.com"
-res = urllib.request.urlopen(url)
-print(res.read())
