c267fb8f77c844b94a7d004e3c6dfc8fbb021b63
Ray <cool.dream@qq.com>
2022-11-19 18:58:15 +0800

    删除练习

--- Crawler_exercises/first_crawl.py
+++
@@ -1,8 +0,0 @@
-from urllib.request import urlopen
-
-url = "http://www.baidu.com"
-resp = urlopen(url)
-with open("mybaidu.html", mode="w", encoding="utf-8") as f:
-    f.write(resp.read().decode("utf-8"))
-print("over")
-resp.close()
